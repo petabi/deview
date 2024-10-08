77a82b231fe45a9583d2067242d0280797f324af
Min Shao <min.shao1988@gmail.com>
2024-10-08 13:49:33 -0700

    Make DigestTable scrollable

--- src/server/tables.rs
+++ src/server/tables.rs
@@ -6,9 +6,10 @@ use dioxus::prelude::*;
 pub fn TableDigest() -> Element {
     rsx! {
         table {
-            style: "table-layout: fixed; overflow: scroll;
-                width: 100%;
-                border-collapse: collapse;",
+            style: "table-layout: fixed;
+                max-width: 100%; max-height: 600px;
+                overflow: auto; display: block;
+                border-spacing: 0;",
             caption {
                 style: "font: small-caps bold 24px sans-serif; text-align: center; border-bottom: 1px solid rgba(0, 0, 0, 0.5)",
                     a {
@@ -19,6 +20,7 @@ pub fn TableDigest() -> Element {
             }
             thead {
                 tr {
+                    style: "position: sticky; top: 0; background: rgba(0, 0, 0, 0.1);",
                     th { style: "width: 200px; text-align: right;", scope: "col", "Table Name" }
                     th { style: "width: 100px; text-align: center;", scope: "col", "Count" }
                     th { scope: "col", "Samples" }
